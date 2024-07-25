412a125762bd4930d9ee9aee3fc72057b984eb86
Homer Riva-Cambrin <neithercouldterry@gmail.com>
2024-07-25 14:20:39 -0600

    added map

--- src/server.rs
+++ src/server.rs
@@ -1,24 +1,33 @@
-use axum::{extract::{Path, State}, response::IntoResponse, routing::get, Router};
-
-
+use axum::{extract::{Path, State}, http::StatusCode, response::IntoResponse, routing::{get, post}, Router};
+use dashmap::DashMap;
 
 #[derive(Clone)]
-pub struct KubeportServer {
+pub struct KubeRoute {
 
+}
+
+
+#[derive(Clone, Default)]
+pub struct KubeportServer {
+    map: DashMap<String, KubeRoute>
 }
 
 impl KubeportServer {
     pub async fn spin() {
-        run_kubeport_server(KubeportServer {}).await;
+        run_kubeport_server(KubeportServer::default()).await;
     }
-    
+    pub fn has_route(&self, key: &str) -> bool {
+        self.map.contains_key(key)
+    }
 }
 
 pub async fn run_kubeport_server(server: KubeportServer) {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/check/:name", get(check_availability))
+        .route("/:service", get(connect_to_route))
+        .route("/available/:name", post(check_availability))
+        .route("/forward/:service", post(connect_to_route))
         .with_state(server);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
@@ -27,8 +36,14 @@ pub async fn run_kubeport_server(server: KubeportServer) {
 
 }
 
-async fn check_availability(State(state): State<KubeportServer>, Path(name): Path<String>) -> impl IntoResponse {
-    
+async fn check_availability(State(state): State<KubeportServer>, Path(name): Path<String>) -> (StatusCode, String)  {
+
+
+    (StatusCode::ACCEPTED, (!state.has_route(&name)).to_string())
+}
+
+async fn connect_to_route(State(state): State<KubeportServer>, Path(name): Path<String>) -> (StatusCode, String) {
+    (StatusCode::ACCEPTED, "hello".to_string())
 }
 
 async fn root() -> &'static str {
